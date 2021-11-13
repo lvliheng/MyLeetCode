575d9ed1c2cafa64664096fd1e32d3e381db610a
lvvi <lvliheng@gmail.com>
2021-11-13 22:59:41 +0800

    ReverseInt

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -11,6 +11,17 @@ public class TwoSum {
 		System.out.println("twoSum: " + Arrays.toString(twoSum(new int[]{1, 2, 3}, 3)));
 	}
 
+	/**
+	 * 1. Two Sum
+	 * 
+	 * Given an array of integers nums and an integer target, r
+	 * eturn indices of the two numbers such that they add up to target.
+	 *
+	 *	You may assume that each input would have exactly one solution, 
+	 *	and you may not use the same element twice.
+	 *	
+	 *	You can return the answer in any order.
+	 */
 	private static int[] twoSum(int[] nums, int target) {
 		
 		Map<Integer, Integer> map = new HashMap<>();
