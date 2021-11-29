83f547b13b0e733cb23404bda9869ca8176bfe0c
lvvi <lvliheng@gmail.com>
2021-11-29 13:42:35 +0800

    Swap Nodes In Pairs

---
+++ src/p0024_Swap_Nodes_In_Pairs/Solution.java
@@ -0,0 +1,67 @@
+package p0024_Swap_Nodes_In_Pairs;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		ListNode inputNode = new ListNode(1);
+		ListNode inputNode1 = new ListNode(2);
+		inputNode.next = inputNode1;
+		ListNode inputNode2 = new ListNode(3);
+		inputNode1.next = inputNode2;
+		ListNode inputNode3 = new ListNode(4);
+		inputNode2.next = inputNode3;
+		
+		ListNode resultListNode = swapPairs2(inputNode);
+		
+		System.out.print("swapPairs: ");
+		
+		while(resultListNode != null) {
+			System.out.print(resultListNode.val + " ");
+	
+			resultListNode = resultListNode.next;
+		}
+	}
+	
+	public static class ListNode {
+	     int val;
+	     ListNode next;
+	     ListNode() {}
+	     ListNode(int val) { this.val = val; }
+	     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	}
+	
+	public static ListNode swapPairs(ListNode head) {
+		ListNode result = new ListNode(0);
+		result.next = head;
+		ListNode pre = result;
+		
+		while(pre.next != null && pre.next.next != null) {
+			ListNode first = pre.next;
+			ListNode second = first.next;
+			
+			first.next = second.next;
+			second.next = first;
+			pre.next = second;
+			
+			pre = first;
+		}
+		
+		return result.next;
+	}
+	
+	public static ListNode swapPairs2(ListNode head) {
+		if(head == null || head.next == null) {
+			return head;
+		}
+		
+		ListNode result = head.next;
+		
+		head.next = swapPairs2(head.next.next);
+		
+		result.next = head;
+		
+		return result;
+	}
+
+}
