baf112f2e0c3cf006209df4ca70293f956b88e1d
lvvi <lvliheng@gmail.com>
2021-12-16 16:20:48 +0800

    Spiral Matrix

---
+++ src/p0054_Spiral_Matrix/Solution.java
@@ -0,0 +1,51 @@
+package p0054_Spiral_Matrix;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		System.out.println("[1,2,3,6,9,8,7,4,5] --> " + spiralOrder(new int[][] {{1,2,3},{4,5,6},{7,8,9}}));
+		System.out.println("[1,2,3,4,8,12,11,10,9,5,6,7] --> " + spiralOrder(new int[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12}}));
+	}
+	
+	public static List<Integer> spiralOrder(int[][] matrix) {
+		if(matrix.length == 0 || matrix[0].length == 0) return new ArrayList<>();
+		
+        List<Integer> result = new ArrayList<>();
+		int m = matrix.length - 1, n = matrix[0].length - 1;
+		int left = 0, top = 0, right = n, bottom = m;
+		
+        
+        while(left <= right && top <= bottom) {
+        	for(int i = left; i <= right; i++) {
+    			result.add(matrix[top][i]);
+    		}
+    		top++;
+    		
+    		if(top > bottom) break;
+    		
+    		for(int j = top; j < bottom; j++) {
+    			result.add(matrix[j][right]);
+    		}
+    		
+    		for(int p = right; p >= left; p--) {
+    			result.add(matrix[bottom][p]);
+    		}
+    		right--;
+    		bottom--;
+    		
+    		if(left > right || top > bottom) break;
+    		
+    		for(int q = bottom; q >= top; q--) {
+    			result.add(matrix[q][left]);
+    		}
+    		left++;
+        }
+		
+		return result;
+    }
+
+}
