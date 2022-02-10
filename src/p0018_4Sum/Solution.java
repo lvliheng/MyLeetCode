359a7b8512ced0bd3eb09afe7e3f21791009dcae
lvvi <lvliheng@gmail.com>
2022-02-10 21:28:18 +0800

    18.4Sum

---
+++ src/p0018_4Sum/Solution.java
@@ -0,0 +1,51 @@
+package p0018_4Sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		System.out.println("[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]] --> " + fourSum(new int[] {1, 0, -1, 0, -2, 2}, 0));
+	}
+	
+	public static List<List<Integer>> fourSum(int[] nums, int target) {
+		List<List<Integer>> result = new ArrayList<>();
+		Arrays.sort(nums);
+		
+		int n = nums.length;
+		
+		for(int i = 0; i < n - 3; i++) {
+			if(i > 0 && nums[i] == nums[i - 1]) continue;
+			
+			for(int j = i + 1; j < n - 2; j++) {
+				if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+				
+				int left = j + 1, right = n - 1;
+				
+				while(left < right) {
+					int sum = nums[i] + nums[j] + nums[left] + nums[right];
+					
+					if(sum == target) {
+						result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+						
+						while(left < right && nums[left] == nums[left + 1]) left++;
+						while(left < right && nums[right] == nums[right - 1]) right--;
+						
+						left++;
+						right--;
+					} else if(sum < target) {
+						left++;
+					} else { 
+						right--;
+					}
+				}
+			}
+		}
+		
+		return result;
+	}
+
+}
